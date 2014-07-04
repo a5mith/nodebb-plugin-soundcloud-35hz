c52378cf89c0a0ca7ec8226ba075b4c10b2edf0e
a5mith <aaronsmith91@sky.com>
2014-07-04 15:12:30 +0100

    amp
    amp

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var SoundCloud = {},
-		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1&amp;color=006CFF&buying=false&download=true"></iframe>';
+		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1&amp;color=006CFF&amp;buying=false&amp;download=true"></iframe>';
 
 
 	SoundCloud.parse = function(postContent, callback) {
