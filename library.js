6c62e0a6c45407a27a867745e4f64cba73210bbc
a5mith <aaronsmith91@sky.com>
2014-07-11 17:10:02 +0100

    back to normal

--- library.js
+++ library.js
@@ -1,17 +1,14 @@
 (function(module) {
-    "use strict";
-    var Soundcloud = {},
-        embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/$2&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>'
+	"use strict";
 
-    Soundcloud.parse = function(postContent, callback) {
-        var	embed = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
+	var SoundCloud = {},
+		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1&amp;color=006CFF&amp;buying=false&amp;download=true"></iframe>';
 
-        if (postContent.match(embed)) {
-            postContent = postContent.replace(embed, embed);
-        }
 
-        callback(null, postContent);
-    };
+	SoundCloud.parse = function(postContent, callback) {
+		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?(.+)">.+<\/a>/g, embed);
+		callback(null, postContent);
+	};
 
-    module.exports = Soundcloud;
+	module.exports = SoundCloud;
 }(module));
