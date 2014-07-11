91af5aef0d9e404ead3502a061d1f2ec6d75fef6
a5mith <aaronsmith91@sky.com>
2014-07-11 17:44:16 +0100

    download on...

--- library.js
+++ library.js
@@ -1,8 +1,8 @@
 (function(module) {
     "use strict";
     var Soundcloud = {},
-        embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/$2&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>',
-        embeds = '<iframe width="100%" height="410" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/sets/$2&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>'
+        embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/$2&amp;color=006CFF&amp;download=true&amp;show_artwork=true"></iframe>',
+        embeds = '<iframe width="100%" height="410" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/sets/$2&amp;color=006CFF&amp;buying=false&amp;download=true&amp;show_artwork=true"></iframe>'
 
     Soundcloud.parse = function(postContent, callback) {
         var	embedtrack = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
