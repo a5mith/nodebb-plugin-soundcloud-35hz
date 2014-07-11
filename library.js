0d2cbf0a243f6fa39b2fdc733686ca967c3c5d8a
a5mith <aaronsmith91@sky.com>
2014-07-11 17:19:20 +0100

    comma doh

--- library.js
+++ library.js
@@ -1,14 +1,22 @@
 (function(module) {
-	"use strict";
+    "use strict";
+    var Soundcloud = {},
+        embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/$2&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>',
+        embeds = '<iframe width="100%" height="410" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/sets/$2&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>'
 
-	var SoundCloud = {},
-		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1&amp;color=006CFF&amp;buying=false&amp;download=true"></iframe>';
+    Soundcloud.parse = function(postContent, callback) {
+        var	embedtrack = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
+        var	embedset = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/sets\/([\w\-_]+)">.+<\/a>/g;
 
+        if (postContent.match(embedtrack)) {
+            postContent = postContent.replace(embedtrack, embed);
+        }
+        if (postContent.match(embedset)) {
+            postContent = postContent.replace(embedset, embeds);
+        }
 
-	SoundCloud.parse = function(postContent, callback) {
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?(.+)">.+<\/a>/g, embed);
-		callback(null, postContent);
-	};
+        callback(null, postContent);
+    };
 
-	module.exports = SoundCloud;
+    module.exports = Soundcloud;
 }(module));
